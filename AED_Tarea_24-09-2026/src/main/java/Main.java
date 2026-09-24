6796ffa7c45791edff6ba4733f1fa29d43423f53
Estebateman <juan.gn1205@gmail.com>
2026-09-24 17:38:02 +0100

    Ejercicio 3

--- AED_Tarea_24-09-2026/src/main/java/Main.java
+++ AED_Tarea_24-09-2026/src/main/java/Main.java
@@ -3,35 +3,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        File fichero = new File("src/main/java/documento.txt");
         Scanner sc = new Scanner(System.in);
-        //Intento 1
+        File ficheros = new File("./ficheros");
+        if (!(ficheros.exists())) { ficheros.mkdir(); }
+        // Ejercicio 1: Copia de un archivo binario mediante bloques de bytes
+        // Ejercicio 2: Contador de bytes específicos en un archivo binario
+        // Ejercicio 3: Escritura directa de texto y modo 'append'
+        File notas = new File("./ficheros/notas.txt");
         try {
-            FileReader lectorBytes = new FileReader(fichero);
-            BufferedReader lector = new BufferedReader(lectorBytes);
-            System.out.println(lector.readLine());
+            if (!(notas.exists())) { notas.createNewFile(); }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        /* Intento 2
         try {
-            FileReader lectorBytes = new FileReader(fichero);
-            while ((lectorBytes.read())!=-1) {
-                System.out.println();
-            }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        } */
-        //Escribir en el archivo
-        try {
-            FileWriter escribidorBytes = new FileWriter(fichero);
-            BufferedWriter escribidor = new BufferedWriter(escribidorBytes);
-            System.out.println("Escribe un verso poético:");
-            String verso = sc.nextLine();
-            escribidor.write(verso);
-            escribidor.close();
-        } catch (Exception e) {
+            FileWriter escritor = new FileWriter(notas);
+            String nota1 = sc.nextLine();
+            escritor.write(nota1);
+        } catch (IOException e) {
             System.out.println(e.getMessage());
         }
+        // Ejercicio 4: Lectura secuencial de caracteres y análisis de texto
     }
 }
